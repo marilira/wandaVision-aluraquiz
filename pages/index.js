25c97b62dfdc25fdf595db258061a20b7eaace39
Mariana Lira <era491@gmail.com>
2021-01-25 21:33:22 -0300

    Aula 1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,54 @@
 import styled from 'styled-components'
+import db from '../db.json';
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizLogo from '../src/components/QuizLogo'
+import Widget from '../src/components/Widget'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>Lorem Impsum</h1>
+          </Widget.Header>
+          
+          <Widget.Content>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero facilis debitis error reiciendis tempore.</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Lorem Impsum</h1>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero facilis debitis error reiciendis tempore.</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/marilira/wandaVision-aluraquiz" />
+    </QuizBackground>
+  );
 }
